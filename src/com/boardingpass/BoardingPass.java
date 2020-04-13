32769d7b82c8c81d15a00f9e886e7c38742d6cbb
Gaurav <gauravnain14@gmail.com>
2020-04-13 15:10:05 -0400

    Update 1

---
+++ src/com/boardingpass/BoardingPass.java
@@ -0,0 +1,127 @@
+package com.boardingpass;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class BoardingPass {
+    private int age, boardingPassNumber;
+    private String name, email, phoneNumber, gender, origin, destination, departureTime, estimatedArrivalTime, strDate;
+    private double ticketPrice;
+
+    public BoardingPass(String name, String email, String phoneNumber, String gender, int age,
+                        String destination, String departureTime){
+        this.boardingPassNumber = ((int)(Math.random()*9999999));
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.age = age;
+        this.origin = "ATL";
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.ticketPrice = setPriceWithAgeAndGender(this.age);
+        this.estimatedArrivalTime = "1h";
+        // pattern "MM/dd/yyyy HH:mm:ss"
+        this.strDate = new SimpleDateFormat("MM/dd/yyyy").format(Calendar.getInstance().getTime());
+    }
+
+    public int getBoardingPassNumber() {
+        return boardingPassNumber;
+    }
+
+    public void setBoardingPassNumber(int boardingPassNumber){
+        this.boardingPassNumber = boardingPassNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getOrigin(){
+        return origin;
+    }
+
+    public void setOrigin(String origin){
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public double setPriceWithAgeAndGender(int theAge){
+        double price = 100.0;
+        if(theAge <= 12)
+            price *= 0.5;
+        else if(theAge >= 60){
+            String getGender = gender.toLowerCase();
+            price *= 0.4;
+            if(getGender.equals("f") || getGender.equals("female"))
+                price *= 0.75;
+        }
+        return price;
+    }
+    public String toString(){
+        return  "BOARDING PASS INFORMATION\n" +
+                "Boarding Pass Number: " + boardingPassNumber + "\n" +
+                "Date: " + strDate + "\n" +
+                "Origin: " + origin + "\n" +
+                "Destination: " + destination + "\n" +
+                "Estimated Arrival Time: " + estimatedArrivalTime + "\n" +
+                "Departure Time: " + departureTime + "\n\n" +
+                "PASSENGER INFORMATION\n" +
+                "Name: " + name + "\n" +
+                "Email: " + email + "\n" +
+                "Phone Number: " + phoneNumber + "\n" +
+                "Gender: " + gender + "\n" +
+                "Age: " + age + "\n\n" +
+                "Total Ticket Price: " + ticketPrice;
+    }
+}
--- src/com/boardingpass/Main.java
+++ src/com/boardingpass/Main.java
@@ -3,7 +3,14 @@ package com.boardingpass;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Testing backend...");
+        String name = "Harry Potter";
+        String email = "harrypotter@hogwarts.com";
+        String phoneNumber = "(123)456-7890";
+        String gender = "M";
+        int age = 30;
+        String destination = "FLL";
+        String departureTime = "21:00:00";
+        BoardingPass hp = new BoardingPass(name, email, phoneNumber, gender, age, destination, departureTime);
+        System.out.println(hp.toString());
     }
 }
