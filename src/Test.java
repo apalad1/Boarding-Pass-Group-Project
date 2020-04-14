277706670e44de2501e27ba0837989575bb1933b
Sterling <sterlingtyson97@gmail.com>
2020-04-14 10:35:57 -0400

    Initial hibernate portion
    Next step is to implement java backend into this code

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,11 @@
+import CRUDQ.CRUDQ;
+import entity.Passenger;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Testing...");
+        System.out.println("Beep boop beep... ... ... Connection test successful!");
+        //Passenger testPass = new Passenger("Mace", "Windu", "mace_windu@swEU.com", "777-888-7878", "05/22/2020", "San Antonio, TX", "Charlotte, NC", "17:05", "14:00");
+        //CRUDQ.createPassenger();
+        //CRUDQ.removePassenger();
     }
 }
