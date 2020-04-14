2237342bec4531af5477658ac3f4cc9735efd5be
Aleck Palad <aleckpalad12@gmail.com>
2020-04-14 17:12:51 -0400

    created frame2

--- src/pasgui.java
+++ src/pasgui.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -18,31 +20,87 @@ public class pasgui extends JFrame{
     private JLabel email_label;
     private JLabel age_label;
     private JLabel gender_label;
-    private JLabel error_label;
+    private JTextField destination_field;
+    private JLabel destination_label;
 
 
     static String name; static String email; static String phone; static String age; static String gender;
+    static String destination;
     JFrame frame2 = new JFrame("PassDisplay");
 
+    //JLabel frame2namelabel = new JLabel("Name: ");
+    JLabel frame2namefield = new JLabel(name);
+
+
+
+    public void panels(){
+        frame2.setSize(500,400);
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame2.setLayout(new GridLayout(3,3));
+
+        JLabel nametxt = new JLabel(name, JLabel.CENTER);               //block for the users name that they put it showing back on the new frame/pass visual
+        nametxt.setLayout(new GridLayout(1,1));
+        nametxt.setFont(new Font("Serif",Font.PLAIN, 16));
+        frame2.add(nametxt);
+
+        JLabel agetxt = new JLabel(age, JLabel.CENTER);                 //block for age
+        agetxt.setLayout(new GridLayout(1,2));
+        agetxt.setFont(new Font("Serif",Font.PLAIN, 16));
+        frame2.add(agetxt);
+
+        JLabel gendertxt = new JLabel(gender, JLabel.CENTER);                 //block for gender
+        gendertxt.setLayout(new GridLayout(1,3));
+        gendertxt.setFont(new Font("Serif",Font.PLAIN, 16));
+        frame2.add(gendertxt);
+
+        JLabel phonetxt = new JLabel(phone, JLabel.CENTER);                 //block for phone
+        phonetxt.setLayout(new GridLayout(2,1));
+        phonetxt.setFont(new Font("Serif",Font.PLAIN, 16));
+        frame2.add(phonetxt);
+
+        JLabel emailtxt = new JLabel(email, JLabel.CENTER);                 //block for phone
+        emailtxt.setLayout(new GridLayout(2,2));
+        emailtxt.setFont(new Font("Serif",Font.PLAIN, 16));
+        frame2.add(emailtxt);
+
+        JLabel idtxt = new JLabel("id", JLabel.CENTER);                 //BOARDING PASS ID IS NEEDED HERE******************************
+        emailtxt.setLayout(new GridLayout(2,3));
+        emailtxt.setFont(new Font("Serif",Font.PLAIN, 16));
+        frame2.add(idtxt);
+
+        JLabel destinationtxt = new JLabel(destination, JLabel.CENTER);                 //BOARDING PASS ID IS NEEDED HERE******************************
+        destinationtxt.setLayout(new GridLayout(3,1));
+        destinationtxt.setFont(new Font("Serif",Font.PLAIN, 16));
+        frame2.add(destinationtxt);
+
+
+    }
+
     public pasgui() {
-        submit_button.addActionListener(new ActionListener() {
+        submit_button.addActionListener(new ActionListener() {              //when the submit is pressed
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Submitting your info");
+
                 name = name_field.getText();
                 age = age_field.getText();
                 email = email_field.getText();
                 phone = phone_field.getText();
                 gender = (String)gender_combobox.getSelectedItem();
+                destination = destination_field.getText();
 
                 //print on console
                 System.out.println(print_to_console(name, age, gender, phone, email));
 
+                JOptionPane.showMessageDialog(null, "Submitting your info");
                 //frame 2 displays pass
-                frame2.setSize(500,400);
+
+                panels();
                 frame2.setVisible(true);
             }
         });
+
+
+
         age_field.addKeyListener(new KeyAdapter() {                 //restrict them from putting in a letter as age
             @Override
             public void keyPressed(KeyEvent e) {
@@ -50,7 +108,7 @@ public class pasgui extends JFrame{
                 String ageanswer = age_field.getText();
                 int len = ageanswer.length();
 
-                if(e.getKeyChar() >= '0' && e.getKeyChar() <= '3'){
+                if(e.getKeyChar() >= '0' && e.getKeyChar() <= '9'){
                     if(len<3){                                          //if youre 1000 years old you cant get on this
                         age_field.setEditable(true);
                     } else{
@@ -92,6 +150,7 @@ public class pasgui extends JFrame{
 
             }
         });
+
 
     }
 
